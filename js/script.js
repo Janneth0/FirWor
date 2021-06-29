21a64978e091b73d0ccdc51b03d458742713a2a5
Janneth0 <churajanneth@gmail.com>
2021-06-29 00:59:50 -0300

    modificando index

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function traerdatos() {
             word.innerHTML = "";
             wordf.innerHTML = "";
 
-                console.log("ingresa a filtro")
+            console.log("ingresa a filtro")
             for (let item of datos) {
                 let cadena = item.clave;
                 let posicion = cadena.indexOf(x);
@@ -23,13 +23,17 @@ function traerdatos() {
 
                 if (posicion >= 0) {
                     wordf.innerHTML += `
-                      <h2>${item.title}</h2><p>${item.def} </p>
+                    <div class="col-3 text-white " style=" background-color:#983DB2ad;">
+      <div class="card-body card-fondo" style=" background-position-y: center; ">  </div>
+          <h3 class="card-title" style="font-family: 'Bebas Neue';  text-align: left;">${item.title}</h3>
+          <p class="card-text1" style="font-family: 'Baloo Tammudu 2', cursive; padding: 5vh 0vh 2vh 0vh;  ">${item.def}</p>
+    </div>
                       `
                     posicion = cadena.indexOf(x);
                 }
 
             }
-            
+
         }
     }
 }
@@ -44,24 +48,28 @@ document.addEventListener('DOMContentLoaded', () => {
         if (this.readyState == 4 && this.status == 200) {
             let x = $("#txtinput").val().toUpperCase();
             let datos = JSON.parse(this.response);
-            
-            word.innerHTML = "";
 
-                console.log(datos)
+            word.innerHTML = `<div class="col-lg-1 "></div>`;
+
+            console.log(datos)
             for (let item of datos) {
                 let cadena = item.clave;
                 let posicion = cadena.indexOf(x);
                 console.log(posicion);
 
                 // if (posicion >= 0) {
-                    word.innerHTML += `
-                      <h2>${item.title}</h2><p>${item.def} </p>
+                word.innerHTML += `
+                <div class="col-lg-3 col-md-4 col-sm-6 col-12  text-white " style=" background-color:#983DB2ad; margin:5px;">
+                <div class="" style=" background-position-y: center; ">  </div>
+                    <h3 class="" style="font-family: 'Bebas Neue';  text-align: left;">${item.title}</h3>
+                    <p class="card-text1" style="font-family: 'Baloo Tammudu 2', cursive; padding: 5vh 0vh 2vh 0vh;  ">${item.def}</p>
+              </div>
                       `
-                    posicion = cadena.indexOf(x);
+                posicion = cadena.indexOf(x);
                 // }
 
             }
-            
+
         }
     }
 
